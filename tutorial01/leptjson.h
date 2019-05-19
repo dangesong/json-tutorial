e1b9b814d8cf574b910af660e61eeba18b1ab178
dangersong <345747751@qq.com>
2019-05-19 11:56:56 +0800

    clang-format code

--- tutorial01/leptjson.h
+++ tutorial01/leptjson.h
@@ -1,17 +1,28 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+typedef enum
+{
+    LEPT_NULL,
+    LEPT_FALSE,
+    LEPT_TRUE,
+    LEPT_NUMBER,
+    LEPT_STRING,
+    LEPT_ARRAY,
+    LEPT_OBJECT
+} lept_type;
 
-typedef struct {
+typedef struct
+{
     lept_type type;
-}lept_value;
+} lept_value;
 
-enum {
+enum
+{
     LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+    LEPT_PARSE_EXPECT_VALUE,      // empty json string
+    LEPT_PARSE_INVALID_VALUE,     // value is invalid
+    LEPT_PARSE_ROOT_NOT_SINGULAR  // some char after whitespace
 };
 
 int lept_parse(lept_value* v, const char* json);
